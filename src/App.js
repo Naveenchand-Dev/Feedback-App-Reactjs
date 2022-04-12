9b872273f6012ea7a1251768f3e15a3ce5c66b76
Naveenchand06 <naveenchand0606@gmail.com>
2022-04-12 23:10:00 +0530

    With add feedback functionality (prop drilling, state management)

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import FeedbackList from "./components/FeedbackList";
 import { useState } from "react";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
+  const [feedbacks, setFeedback] = useState(FeedbackData);
 
   const deleteFeedback = (id) => {
     console.log("App", id);
     if (window.confirm("Are you sure you want to delete this feedback?")) {
-      setFeedback(feedback.filter((item) => item.id !== id));
+      setFeedback(feedbacks.filter((item) => item.id !== id));
     }
+  };
+
+  const addFeedback = (newFeedback) => {
+    console.log(newFeedback);
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedbacks]);
   };
 
   return (
@@ -21,8 +29,9 @@ function App() {
       {/* <Header bgColor="red" textColor="blue" /> */}
       <Header />
       <div className="container">
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <FeedbackForm handleAdd={addFeedback} />
+        <FeedbackStats feedback={feedbacks} />
+        <FeedbackList feedback={feedbacks} handleDelete={deleteFeedback} />
       </div>
     </>
   );
